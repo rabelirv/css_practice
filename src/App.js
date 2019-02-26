b76da5dda572e139bcbf4fab7f6516e588b56a93
github username <github email address>
2019-02-26 00:24:45 -0500

    practice css

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return (<div className="App">
+    <head>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"/>
+    </head>
+      <body>
+        <nav>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">about</a></li>
+            <li><a href="#">contact</a></li>
+          </ul>
+        </nav>
+        <main>
+        <h1>Let's Go Exploring</h1>
+        <p> Come with me! Do you wannna ride or die? ddadadadadadada</p>
+        <a href="#"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStfMq_IJ_LbXm6dYOko7_VyHs2QM5JoP2f2AM-2ZSxJ-brO42bZA" alt="left arrow"/></a>
+        </main>
+      </body>
+    </div>);
   }
 }
 
